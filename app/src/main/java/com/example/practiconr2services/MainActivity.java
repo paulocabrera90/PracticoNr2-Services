5052849c321d91105d69c1a69408159ef59b5b11
Paulo <paulo.cabrera@atos.net>
2021-09-01 02:01:37 -0300

    Servicios de capturas de mensajes

--- app/src/main/java/com/example/practiconr2services/MainActivity.java
+++ app/src/main/java/com/example/practiconr2services/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.practiconr2services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent intentService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkCallingOrSelfPermission(Manifest.permission.READ_SMS) != PackageManager.PERMISSION_DENIED){
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, 1000);
+        }
+        //instancio un intent de la clase q extiende Servicio
+        intentService = new Intent(this, MensajeService.class);
+        //Inicio el servicio
+        startService(intentService);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(intentService);// dentengo el servicio
+
     }
 }
---
+++ app/src/main/java/com/example/practiconr2services/MensajeService.java
@@ -0,0 +1,53 @@
+package com.example.practiconr2services;
+
+import android.app.Service;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.IBinder;
+import android.provider.Telephony;
+import android.util.Log;
+
+public class MensajeService extends Service {
+    public MensajeService() {
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Uri dbMensaje = Uri.parse("content://sms/inbox");
+        ContentResolver cr = getContentResolver();
+        while(true) {
+            Cursor cursor = cr.query(dbMensaje,null,null,null,null);
+            String id = null;
+            String msn = null;
+            if (cursor != null && cursor.getCount() > 0) {
+                int contador = 0;
+                while (cursor.moveToNext() && contador < 5) {
+                    id = cursor.getString(cursor.getColumnIndex(Telephony.Sms.ADDRESS));
+                    msn = cursor.getString(cursor.getColumnIndex(Telephony.Sms.BODY));
+                    Log.d("Mensaje", id + " " + msn);
+                    contador++;
+                }
+                try {
+                    Thread.sleep(9000);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+            cursor.close();
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
